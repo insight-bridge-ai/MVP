2753f872c906df64ef580b1058a1a55977bffa75
samsonkebede4 <samsonkebede4@gmail.com>
2025-10-31 09:13:32 +0300

    Fix:Cors errors

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from services.router import llm_router
 import os
 app = FastAPI()
 
+
+app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
